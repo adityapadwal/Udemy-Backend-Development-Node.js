7e69426e5483b2ab7e494b2d9f2dd43438e6a1ff
Aditya-Padwal <aditya.padwal3102@gmail.com>
2022-10-20 16:13:01 +0530

    Oct 20 2022

--- Section03- Basics of Node.js/routes.js
+++ Section03- Basics of Node.js/routes.js
@@ -21,7 +21,7 @@ const requestHandler = (req, res) =>{
             body.push(chunk);
         });
         return req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
+            const parsedBody  = Buffer.concat(body).toString();
             const message = parsedBody.split('=')[1];
             fs.writeFile('message.txt', message, (err) => {
                 res.statusCode = 302;  // redirection
@@ -32,11 +32,12 @@ const requestHandler = (req, res) =>{
         });
     }
     
+    
     res.setHeader("Content-Type", "text/html");
     res.write("<html>");
     res.write("<head> <title> My First Page </title> </head>");
-    res.write("<body> <h1> Hello form my Node.js server! </h1> </body>")
-    res.write("</html>")
+    res.write("<body> <h1> Hello form my Node.js server! </h1> </body>");
+    res.write("</html>");
     res.end();
 }
 
