6e6a9d1c96621b5dc2de7f83e5ecc31f7c0761d6
Aditya-Padwal <aditya.padwal3102@gmail.com>
2022-11-18 18:56:51 +0530

    Oct 18 2022

--- Section05- Express.js/app.js
+++ Section05- Express.js/app.js
@@ -1,24 +1,25 @@
 // Import express 
 const express = require('express');
+// Inporting body-parser
+const bodyParser = require('body-parser');
 
 // Creating an express application and putting it in a const
 // by running it as a function 
 // this app constant also happens to be a valid request handler
 const app = express();
 
-// .use() allows us to add a new middleware function
-app.use('/', (req, res, next) => {
-    console.log('This always runs! ');
-    next();
-})
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/add-product',(req, res, next) => {
-    console.log('In add-product middleware');
-    res.send('<h1>The Add Product Page</h1>');
+    res.send('<form action="/product" method="POST"> <input type="text" name="title"> <button type="submit">Add Product</button> </form>');
+});
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
 app.use('/',(req, res, next) => {
-    console.log('In the middleware! ');
     res.send('<h1>Hello from express.js</h1>');
 });
 
