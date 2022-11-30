b298ae629ccf28e5449c1d6af86523cb77d7c062
Aditya-Padwal <aditya.padwal3102@gmail.com>
2022-11-30 19:58:10 +0530

    Nov 30 2002

---
+++ Assignments/Section05 Assignment02/routes/users.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const rootdir = require('../util/path');
+
+const router = express.Router();
+
+router.get('/users', (req, res, next) => {
+    res.sendFile(path.join(rootdir, 'views', 'users.html'));
+});
+
+module.exports = router;
---
+++ Assignments/Section05 Assignment02/util/path.js
@@ -0,0 +1,4 @@
+// Importing the path core module
+const path = require('path');
+
+module.exports = path.dirname(process.mainModule.filename);
