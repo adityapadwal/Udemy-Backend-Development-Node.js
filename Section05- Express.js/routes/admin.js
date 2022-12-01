ad2ed3478fbb45929702c280f38cba16fa18ae2b
Aditya-Padwal <aditya.padwal3102@gmail.com>
2022-12-01 17:23:09 +0530

    Dec 01 2022

--- Section05- Express.js/routes/admin.js
+++ Section05- Express.js/routes/admin.js
@@ -1,23 +1,33 @@
 // Importing express
 const express = require("express");
 
-const rootDir = require('../util/path'); // to work on all operating systems
+const rootDir = require("../util/path"); // to work on all operating systems
 
 // Importing the path core module
-const path = require('path'); 
+const path = require("path");
 
 // creating a router
 const router = express.Router();
 
+const products = [];
+
 // This will match to /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
 }); // __dirname points to the routes folder
 
 // This will match to /admin/add-product => POST
 router.post("/add-product", (req, res, next) => {
-  console.log(req.body);
+  products.push({ title: req.body.title });
   res.redirect("/");
 });
 
-module.exports = router;
+// module.exports = router;
+exports.routes = router;
+exports.products = products;
--- Section05- Express.js/routes/shop.js
+++ Section05- Express.js/routes/shop.js
@@ -1,18 +1,27 @@
 // Importing express.js
-const express = require('express');
+const express = require("express");
 
-const rootDir = require('../util/path');  // to work on all operating systems  
+const rootDir = require("../util/path"); // to work on all operating systems
+const adminData = require("./admin.js");
 
 // Importing the path core module
-const path = require('path');
+const path = require("path");
 
 // Creating an express.js router
 const router = express.Router();
 
-router.get('/',(req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-}); 
+router.get("/", (req, res, next) => {
+  const products = adminData.products;
+  res.render("shop", {
+    prods: products,
+    pageTitle: "Shop",
+    path: "/",
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
+    // layout: false
+  });
+});
 // In the above code __dirname points to routes folder
 
 module.exports = router;
-
