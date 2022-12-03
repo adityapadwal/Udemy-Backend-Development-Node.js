0eb1735638201bd643dc8516a02f54eedd2a2d9e
Aditya-Padwal <aditya.padwal3102@gmail.com>
2022-12-03 14:08:28 +0530

    Dec 03 2022

--- Section07- Model View Controller (MVC)/controllers/products.js
+++ Section07- Model View Controller (MVC)/controllers/products.js
@@ -1,4 +1,4 @@
-const products = [];
+const Product = require('../models/product.js');
 
 exports.getAddProducts = (req, res, next) => {
     res.render("add-product", {
@@ -11,19 +11,22 @@ exports.getAddProducts = (req, res, next) => {
   }
 
 exports.postAddProducts = (req, res, next) => {
-    products.push({ title: req.body.title });
+    const product = new Product(req.body.title);
+    product.save();
     res.redirect("/");
   }
 
 exports.getProducts = (req, res, next) => {
-    res.render("shop", {
+  Product.fetchAll( products => {
+    res.render('shop', {
       prods: products,
-      pageTitle: "Shop",
-      path: "/",
+      pageTitle: 'Shop',
+      path: '/',
       hasProducts: products.length > 0,
       activeShop: true,
       productCSS: true,
       // layout: false
     });
     // console.log(products);
-  }
+  });
+  };
--- Section07- Model View Controller (MVC)/util/path.js
+++ Section07- Model View Controller (MVC)/util/path.js
@@ -1,5 +1,5 @@
 // Importing the path core module 
 const path = require('path');
 
-module.exports = path.dirname(process.mainModule.filename);
+module.exports = path.dirname(require.main.filename);
 
