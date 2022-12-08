09ea2b0f7a0190c0e50ebf5de79b5ef6cdd845b3
Aditya-Padwal <aditya.padwal3102@gmail.com>
2022-12-08 18:11:18 +0530

    Section 09 Lecture 127

---
+++ Section09 - Dynamic Routes and Advanced Models/routes/shop.js
@@ -0,0 +1,28 @@
+// Importing express.js
+const express = require("express");
+
+// Importing the path core module
+const path = require('path');
+const { resourceLimits } = require("worker_threads");
+
+// Importing products controller
+const shopController = require('../controllers/shop');
+
+// Creating an express.js router
+const router = express.Router();
+
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/products/:productId', shopController.getProduct);
+
+router.get('/cart', shopController.getCart);
+
+router.post('/cart', shopController.postCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
+
+module.exports = router;
