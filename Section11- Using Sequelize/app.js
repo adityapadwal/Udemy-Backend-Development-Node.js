6038c25dd3834589a92f1c00106f5ffe28b3b51a
Aditya-Padwal <aditya.padwal3102@gmail.com>
2022-12-18 19:05:47 +0530

    Section 11

--- Section11- Using Sequelize/app.js
+++ Section11- Using Sequelize/app.js
@@ -21,6 +21,8 @@ const Product = require("./models/product");
 const User = require("./models/user");
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-items');
 
 const app = express();
 
@@ -57,6 +59,9 @@ User.hasOne(Cart);
 Cart.belongsTo(User);                                                         // This will add userId to the cart table
 Cart.belongsToMany(Product, {through: CartItem});
 Product.belongsToMany(Cart, {through: CartItem});
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, {through: OrderItem});
 
 sequelize
   // .sync({force: true})
